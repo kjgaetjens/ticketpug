24a96136f046088b0d5582acaeb6dfe3f00b023f
Elizabeth Vasquez <vasquezelizabeth03@gmail.com>
2019-08-23 17:26:45 -0500

    search page styling

--- routes/search.js
+++ routes/search.js
@@ -27,7 +27,7 @@ router.get('/', async (req,res)=>{
     //         })
        console.log(eventinfo)
        
-       res.render('search', {event: eventinfo})
+       res.render('search', {event: eventinfo, result: search})
     })
 
     })
