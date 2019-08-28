805c809f0856b69399bc03f56d1608812a2e86a3
Elizabeth Vasquez <vasquezelizabeth03@gmail.com>
2019-08-28 12:24:57 -0500

    condensed api call

--- routes/venues.js
+++ routes/venues.js
@@ -20,28 +20,11 @@ router.get('/:venueid/events', (req,res)=>{
     let venueId = req.params.venueid
     axios.get(`https://app.ticketmaster.com/discovery/v2/events?apikey=GgkMBDROaaG6jddcy0k07d6GGEyYG4gE&venueId=${venueId}&locale=*&classificationName=music`)
         .then(response =>{
-            let eventInfo = response.data._embedded.events.map(event=>{
-                return {
-                eventid: event.id,
-                eventname: event.name,
-                musician: event._embedded.attractions ? event._embedded.attractions[0].name : "N/A",
-                musicianid: event._embedded.attractions ? event._embedded.attractions[0].id : "N/A",
-                date: event.dates.start.localDate,
-                time: event.dates.start.localTime,
-                venue: event._embedded.venues[0].name,
-                venuecity: event._embedded.venues[0].city.name,
-                venuestate: event._embedded.venues[0].state.stateCode,
-                venueaddress: event._embedded.venues[0].address.line1,
-                venueid: event._embedded.venues[0].id,
-                seatmap: event.seatmap ? event.seatmap.staticUrl : "N/A"
-                }
-            
-            })
-            let venue = eventInfo[0].venue
+            let eventInfo = response.data._embedded.events
+            let venue = eventInfo[0]._embedded.venues[0]
             console.log(venue)
-            res.render("venueevents", {event: eventInfo, venueInfo: venue})
-        }).catch(e=>console.log(e))
-})
-
+            res.render("venueevents", {event: eventInfo, venue: venue})
+            }).catch(e=>console.log(e))
+        })
 module.exports = router
 
